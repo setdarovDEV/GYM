eff9295760508a2f7dba022df817c197bfdf6b16
ZafarNishonov <nishonovofficial@gmail.com>
2025-12-24 12:15:23 +0500

    start menu: Menu, Profile, Cart, Last Orders

--- app/bot/buttons/reply.py
+++ app/bot/buttons/reply.py
@@ -1,6 +1,16 @@
+from aiofiles.os import replace
 from aiogram.types import KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+def start_button():
+    menyu = KeyboardButton(text='🥗 Menyu')
+    profilim = KeyboardButton(text='👤 Profilim ')
+    savat = KeyboardButton(text='🛒 Savat')
+    oxirgi = KeyboardButton(text='📜 Oxirgi Buyurtma')
+    reply = ReplyKeyboardBuilder()
+    reply.add(menyu, profilim, savat, oxirgi)
+    reply.adjust(2, 2)
+    return reply.as_markup(resize_keyboard=True)
 
 def menu_buttons():
     ozish = KeyboardButton(text='🔥 Ozish ')
