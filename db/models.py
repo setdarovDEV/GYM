e3cd99d9bde3adc7f41793437862f27741058f04
dilshodev-py <absaitovdev@gmail.com>
2025-12-24 10:14:59 +0500

    shablon push db

--- db/models.py
+++ db/models.py
@@ -1,1 +1,21 @@
 # comment
+
+# class Base:
+#     table_name
+
+
+# class Table1(Base):
+#     pass
+#
+# class Table1(Base):
+#     pass
+#
+# class Table1(Base):
+#     pass
+#
+#
+# class Table1(Base):
+#     pass
+#
+# class Table1(Base):
+#     pass
