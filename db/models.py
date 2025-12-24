2cab19030bf2d37528ebac2e485d19463ceec205
baxritdinovbdev-pixel <baxritdinovbdev@gmail.com>
2025-12-24 10:40:45 +0500

    Models

--- db/models.py
+++ db/models.py
@@ -7,6 +7,7 @@ engine.connect()
 class Base(DeclarativeBase):
     pass
 
+
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
@@ -14,22 +15,51 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(String(255))
     phone_number: Mapped[str] = mapped_column(String(50))
 
-# class Base:
-#     table_name
+    locations: Mapped[list["UserLocation"]] = relationship(back_populates="user", cascade="all, delete")
+    orders: Mapped[list["Order"]] = relationship(back_populates="user")
 
 
-# class Table1(Base):
-#     pass
-#
-# class Table1(Base):
-#     pass
-#
-# class Table1(Base):
-#     pass
-#
-#
-# class Table1(Base):
-#     pass
-#
-# class Table1(Base):
-#     pass
+class UserLocation(Base):
+    __tablename__ = "user_locations"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"))
+    title: Mapped[str] = mapped_column(String)
+    longitude: Mapped[float] = mapped_column(Numeric)
+    latitude: Mapped[float] = mapped_column(Numeric)
+
+    user: Mapped["User"] = relationship(back_populates="locations")
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    products: Mapped[list["Product"]] = relationship(back_populates="category")
+
+
+class ProductStatus(enum.Enum):
+    active = "active"
+    inactive = "inactive"
+
+
+# =========================
+# PRODUCTS
+# =========================
+
+class Product(Base):
+    __tablename__ = "products"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="NO ACTION"))
+    name: Mapped[str] = mapped_column(String(100))
+    photo: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(Text)
+    price: Mapped[int] = mapped_column(Numeric(12, 0))
+    quantity: Mapped[int] = mapped_column(SmallInteger)
+
+    status: Mapped[ProductStatus] = mapped_column(Enum(ProductStatus, name="product_status"),default=ProductStatus.active)
+
+    category: Mapped["Category"] = relationship(back_populates="products")
+
+
